51934cb7cf8ead4b43fe880e0be054867653b3f6
BWart <bastian.wartmann@digitecgalaxus.ch>
2021-06-29 21:29:15 +0200

    Contact Form Info Text updated and with border

--- assets/dynamicWording.js
+++ assets/dynamicWording.js
@@ -91,7 +91,7 @@ function filldynamicWording(){
         'recommendedChannel' : 'Empfohlener Kanal',
         'searchBoxText' : '<h2><font color=#00000>Wie können wir dir helfen?</font></h2>', 
         'infoNoteText' : '<br>Aufgrund der aktuellen Wetter- bzw. Strassensituation ist mit Verspätungen bei der Paketzustellung durch die Post zu rechnen. Besten Dank für dein Verständnis<br>&nbsp;',
-        'contactFormInfoText' : 'Gerne stehen wir dir per Telefon, Chat und Kontaktformular zur Verfügung. Um dein Anliegen rasch bearbeiten zu können, benötigen wir noch ein paar Informationen von dir.',
+        'contactFormInfoText' : 'Suchst du unsere Kontaktangaben? Füll das Formular aus, damit wir dein Anliegen rasch bearbeiten können und wähle dann, ob du per Telefon, Chat oder via Formular mit uns in Kontakt treten willst.',
         'chatNotOnline': 'Nicht verfügbar'
         },
         fr: {      
@@ -122,7 +122,7 @@ function filldynamicWording(){
         'recommendedChannel' : 'Canal recommandé',
         'searchBoxText' : '<h2><font color=#00000>Comment pouvons-nous vous aider?</font></h2>',
         'infoNoteText' : '<br>En raison des conditions météorologiques et de la situation routière actuelles, il faut s\'attendre à des retards dans la livraison des colis par la Poste suisse. Merci de votre compréhension<br>&nbsp;',
-        'contactFormInfoText' : 'Nous sommes heureux de vous aider par téléphone, chat et formulaire de contact. Afin de traiter votre demande rapidement, nous avons encore besoin de certaines informations de votre part.',
+        'contactFormInfoText' : 'Vous cherchez nos coordonnées ? Remplissez le formulaire afin que nous puissions traiter votre demande rapidement. Choisissez ensuite si vous souhaitez nous contacter par téléphone, par chat ou par formulaire.',
         'chatNotOnline': 'Non disponible'
         },
         it: {
@@ -153,7 +153,7 @@ function filldynamicWording(){
         'recommendedChannel' : 'Canale consigliato',
         'searchBoxText' : '<h2><font color=#00000>Come possiamo aiutarla?</font></h2>',
         'infoNoteText' : '<br>A causa dell\'attuale situazione meteorologica e stradale, si prevedono ritardi nella consegna dei pacchi da parte della Posta Svizzera. Grazie per la vostra comprensione<br>&nbsp;',
-        'contactFormInfoText' : 'Siamo felici di assistervi per telefono, chat e modulo di contatto. Al fine di elaborare rapidamente la sua richiesta, abbiamo ancora bisogno di alcune informazioni da lei.',
+        'contactFormInfoText' : 'Stai cercando i nostri contatti? Compila il modulo in modo che possiamo elaborare rapidamente la tua richiesta e poi scegli se vuoi contattarci per telefono, chat o tramite modulo.',
         'chatNotOnline': 'Non disponible'
         },
         'en-US': {
@@ -184,7 +184,7 @@ function filldynamicWording(){
         'recommendedChannel' : 'Recommended channel',
         'searchBoxText' : '<h2><font color=#00000>How can we help you?</font></h2>',
         'infoNoteText' : '<br>Due to the current weather and road situation, delays in parcel delivery by Swiss Post are to be expected. Thank you for your understanding<br>&nbsp;',
-        'contactFormInfoText' : 'We are happy to assist you by phone, chat and contact form. In order to process your request quickly, we need some information from you.',
+        'contactFormInfoText' : 'Are you looking for our contact details? Fill out the form so that we can quickly process your request and then choose whether you want to contact us by phone, chat or via form.',
         'chatNotOnline': 'Not available'
         }
     }
