669df8c9be3029035ca4aff633957804b00b9a6e
Camillo Schweizer <camillo.schweizer@digitecgalaxus.ch>
2021-08-03 17:56:34 +0200

    more department changes

--- assets/chat.js
+++ assets/chat.js
@@ -8,67 +8,74 @@ function waitForChat(){
 }
 
 
+///////////////////////////////////////////////////////////Widget/////////////////////////////////////////////////////////
+
+// Wechselt widget anhand des Typs (Set & Update)
 function changeWebWidgetSetting(type){
+    console.log('beforwebwidgetsettings')
     console.log(getWebWidgetSettings())
     if (type == 'set'){
         window.zESettings = getWebWidgetSettings();
     }else{
         zE('webWidget', 'updateSettings', getWebWidgetSettings());
     }
-
 }
 
-
+// Definiert die Widget Settings
 function getWebWidgetSettings(){
     var dep = getChatDepartment();
+
     var zeSettings = {
         webWidget: {
             chat: {
-              suppress: false,
-              departments: {
-                select: dep,
-                enabled: []
-              },
-               title: {
-                '*': 'Chat with us',
-                fr: 'Chattez avec nous',
-                de: "Chatte mit uns",
-                it: "Chattata con noi"
-              },
-              prechatForm: {
-              greeting: {
-                '*': 'Please have your order, service or invoice number ready so that we can help you even faster.',
-                fr: "Veuillez préparer votre numéro de commande, de service ou de facture afin que nous puissions vous aider encore plus rapidement. ",
-                de: "Bitte halte deine Bestell-, Service- oder Rechnungsnummer bereit, damit wir dir noch schneller helfen können. ",
-                it: "Tieni pronto il tuo numero d'ordine, di servizio o di fattura in modo che possiamo aiutarti ancora più velocemente. "
-              }
-            },
-              offlineForm: {
-              greeting: {
-                '*': "We aren't online right now",
-                fr: "Nous ne sommes pas en ligne pour le moment",
-                en: "We aren't online right now",
-                de: "Wir sind im Moment nicht online",
-                it: "Non siamo online in questo momento"
-              }
-            }
+                suppress: false,
+                departments: {
+                    select: dep,
+                    enabled: []
+                },
+                title: {
+                    '*': 'Chat with us',
+                    fr: 'Chattez avec nous',
+                    de: "Chatte mit uns",
+                    it: "Chattata con noi"
+                },
+                prechatForm: {
+                    greeting: {
+                        '*': 'Please have your order, service or invoice number ready so that we can help you even faster.',
+                        fr: "Veuillez préparer votre numéro de commande, de service ou de facture afin que nous puissions vous aider encore plus rapidement. ",
+                        de: "Bitte halte deine Bestell-, Service- oder Rechnungsnummer bereit, damit wir dir noch schneller helfen können. ",
+                        it: "Tieni pronto il tuo numero d'ordine, di servizio o di fattura in modo che possiamo aiutarti ancora più velocemente. "
+                    }
+                },
+                offlineForm: {
+                    greeting: {
+                        '*': "We aren't online right now",
+                        fr: "Nous ne sommes pas en ligne pour le moment",
+                        en: "We aren't online right now",
+                        de: "Wir sind im Moment nicht online",
+                        it: "Non siamo online in questo momento"
+                    }
+                }
             },
             contactForm: {
-              suppress: true
+                suppress: true
             },
             helpCenter: {
-              suppress: true
+                suppress: true
             },
             talk: {
-              suppress: true
+                suppress: true
             },
             answerBot: {
-              suppress: true
-          }
+                suppress: true
+            }
         }
     }
     return zeSettings;
 }
+
+
+/////////////////////////////////////////////////////////////Departments///////////////////////////////////////////////////////////////////////////
 
 // Gibt das Chat Department anhand der Sprache und des Kundentyps zurück
 function getChatDepartment(){
