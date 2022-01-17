1514f36893bf69ca0b83a2c0377acd13b5cce2c4
VapidRush <jannikheger@googlemail.com>
2022-01-17 12:38:27 +0100

    Banner

---
+++ assets/topBannerWording.js
@@ -0,0 +1,60 @@
+function fillTopBannerWording(){
+  
+    topBannerWording = {
+
+    //Banner
+    "outputHeadingBannerDGCH" : {
+      'de' : "Achtung",
+      'fr' : "Attention",
+      'it' : "Attenzione",
+      'en-US' : "Attention"
+    },
+    "outputTextBannerDGCH" : {
+      'de' : "Der Versand beträgt momentant 5-7 Tage.",
+      'fr' : "Les délais de livraison sont actuellement de 5 à 7 jours.",
+      'it' : "Il tempo di spedizione è attualmente di 5-7 giorni.",
+      'en-US' : "The shipping time is 5-7 days at the moment."
+    },
+    "outputHeadingBannerGLCH" : {
+      'de' : "Achtung",
+      'fr' : "Attention",
+      'it' : "Attenzione",
+      'en-US' : "Attention"
+    },
+    "outputTextBannerGLCH" : {
+      'de' : "Der Versand beträgt momentant 15-17 Tage.",
+      'fr' : "Les délais de livraison sont actuellement de 15 à 17 jours.",
+      'it' : "Il tempo di spedizione è attualmente di 15-17 giorni.",
+      'en-US' : "The shipping time is 15-17 days at the moment."
+    },
+    "outputHeadingBannerGLDE" : {
+      'de' : "Achtung",
+      'fr' : "Attention",
+      'it' : "Attenzione",
+      'en-US' : "Attention"
+    },
+    "outputTextBannerGLDE" : {
+      'de' : "Der Versand beträgt momentant 25-27 Tage.",
+      'fr' : "Les délais de livraison sont actuellement de 25 à 27 jours.",
+      'it' : "Il tempo di spedizione è attualmente di 25-27 giorni.",
+      'en-US' : "The shipping time is 25-27 days at the moment."
+    },
+    "outputHeadingBannerGLAT" : {
+      'de' : "Achtung",
+      'fr' : "Attention",
+      'it' : "Attenzione",
+      'en-US' : "Attention"
+    },
+    "outputTextBannerGLAT" : {
+      'de' : "Der Versand beträgt momentant 35-37 Tage.",
+      'fr' : "Les délais de livraison sont actuellement de 35 à 37 jours.",
+      'it' : "Il tempo di spedizione è attualmente di 35-37 giorni.",
+      'en-US' : "The shipping time is 35-37 days at the moment."
+    },
+    
+    }
+
+
+      return topBannerWording;
+}
+
