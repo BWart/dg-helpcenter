53262919ccd32c275c9c47de3678f36a4590602e
VapidRush <jannikheger@googlemail.com>
2022-02-22 11:15:49 +0100

    Banner

--- assets/topBannerWording.js
+++ assets/topBannerWording.js
@@ -3,49 +3,37 @@ function fillTopBannerWording(){
     topBannerWording = {
 
     //Banner
-    "outputHeadingBannerDGCH" : {
+    "outputHeadingBannerCH" : {
       'de' : "Achtung",
       'fr' : "Attention",
       'it' : "Attenzione",
       'en-US' : "Attention"
     },
-    "outputTextBannerDGCH" : {
+    "outputTextBannerCH" : {
       'de' : "Der Versand beträgt momentant 5-7 Tage.",
       'fr' : "Les délais de livraison sont actuellement de 5 à 7 jours.",
       'it' : "Il tempo di spedizione è attualmente di 5-7 giorni.",
       'en-US' : "The shipping time is 5-7 days at the moment."
     },
-    "outputHeadingBannerGLCH" : {
+    "outputHeadingBannerDE" : {
       'de' : "Achtung",
       'fr' : "Attention",
       'it' : "Attenzione",
       'en-US' : "Attention"
     },
-    "outputTextBannerGLCH" : {
-      'de' : "Der Versand beträgt momentant 15-17 Tage.",
-      'fr' : "Les délais de livraison sont actuellement de 15 à 17 jours.",
-      'it' : "Il tempo di spedizione è attualmente di 15-17 giorni.",
-      'en-US' : "The shipping time is 15-17 days at the moment."
-    },
-    "outputHeadingBannerGLDE" : {
-      'de' : "Achtung",
-      'fr' : "Attention",
-      'it' : "Attenzione",
-      'en-US' : "Attention"
-    },
-    "outputTextBannerGLDE" : {
+    "outputTextBannerDE" : {
       'de' : "Der Versand beträgt momentant 25-27 Tage.",
       'fr' : "Les délais de livraison sont actuellement de 25 à 27 jours.",
       'it' : "Il tempo di spedizione è attualmente di 25-27 giorni.",
       'en-US' : "The shipping time is 25-27 days at the moment."
     },
-    "outputHeadingBannerGLAT" : {
+    "outputHeadingBannerAT" : {
       'de' : "Achtung",
       'fr' : "Attention",
       'it' : "Attenzione",
       'en-US' : "Attention"
     },
-    "outputTextBannerGLAT" : {
+    "outputTextBannerAT" : {
       'de' : "Der Versand beträgt momentant 35-37 Tage.",
       'fr' : "Les délais de livraison sont actuellement de 35 à 37 jours.",
       'it' : "Il tempo di spedizione è attualmente di 35-37 giorni.",
@@ -53,7 +41,6 @@ function fillTopBannerWording(){
     },
     
     }
-
 
       return topBannerWording;
 }
