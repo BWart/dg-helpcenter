7656d0db4428114a24c821e5123034ac2fb2f910
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2022-04-13 15:29:03 +0200

    holidays easter

--- assets/holidays.js
+++ assets/holidays.js
@@ -6,24 +6,24 @@
 //2. - ID of the text in textToDisplay 
 var showHolidays = {
     'ch' : [
-        ['2021-12-30 00:00:00', '2021-12-31 23:00:00', 0]
+        ['2022-04-13 00:00:00', '2022-04-18 22:00:00', 0]
     ],
     'eu' : [
-        ['2021-12-30 00:00:00', '2022-01-01 23:00:00', 1]
+        ['2022-04-14 00:00:00', '2022-04-18 22:00:00', 1]
     ]
 }
 var textToDisplay = {
     0 : {
-        'de' : '<br>31.12.2021: Ab 16:00 Uhr geschlossen',
-        'fr' : '<br>31.12.2021 : fermé à partir de 16h00',
-        'it' : '<br>31.12.2021: Chiuso dalle 16:00',
-        'en-US' : '<br>31.12.2021: Closed from 16:00',
+        'de' : '<br>Ostern: 14.04.2022 ab 16:00 Uhr bis 18.04.2022 geschlossen',
+        'fr' : "<br>Pâques : 14.04.2022 à partir de 16:00 heures jusqu'au 18.04.2022 fermé<br>",
+        'it' : '<br>Pasqua: 14.04.2022 chiuso dalle 16:00 al 18.04.2022<br>',
+        'en-US' : '<br>Easter: 14.04.2022 from 16:00 until 18.04.2022 closed<br>',
     },
     1 : {
-        'de' : '<br>31.12.2021: Ab 16:00 Uhr geschlossen<br>01.01.2022: Geschlossen',
-        'fr' : '<br>31.12.2021 : fermé à partir de 16h00<br>01.01.2022 : Fermé',
-        'it' : '<br>31.12.2021: Chiuso dalle 16:00<br>01.01.2022: Chiuso',
-        'en-US' : '<br>31.12.2021: Closed from 16:00<br>01.01.2022: Closed',
+        'de' : '<br>Ostern: 15.04.2022 & 18.04.2022 geschlossen',
+        'fr' : '<br>Pâques : 15.04.2022 & 18.04.2022 fermé',
+        'it' : '<br>Pasqua: 15.04.2022 & 18.04.2022 chiuso',
+        'en-US' : '<br>Easter: 15.04.2022 & 18.04.2022 closed',
     }
 }
 
@@ -33,9 +33,9 @@ var textToDisplay = {
 //1. - ending date to stop hiding waiting times
 var hideWaitingTimes = {
     'ch' : [
-        ['2021-12-31 15:00:00', '2022-01-01 23:00:00']
+        ['2022-04-14 14:00:00', '2022-04-18 22:00:00']
     ],
     'eu' : [
-        ['2021-12-31 13:00:00', '2022-01-01 23:00:00']
+        ['2022-04-15 00:00:00', '2022-04-15 22:00:00'], ['2022-04-18 00:00:00', '2022-04-18 22:00:00']
     ]
 }
