ec43bf95b58fa8432180e4af0e28909c051de1c2
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2022-05-25 10:18:57 +0200

    Ascension closing times added

--- assets/holidays.js
+++ assets/holidays.js
@@ -6,24 +6,24 @@
 //2. - ID of the text in textToDisplay 
 var showHolidays = {
     'ch' : [
-        ['2022-04-13 00:00:00', '2022-04-18 22:00:00', 0]
+        ['2022-05-25 00:00:00', '2022-05-26 22:00:00', 0]
     ],
     'eu' : [
-        ['2022-04-13 00:00:00', '2022-04-18 22:00:00', 1]
+        ['2022-05-25 00:00:00', '2022-05-26 22:00:00', 1]
     ]
 }
 var textToDisplay = {
     0 : {
-        'de' : '<br>Ostern: Geschlossen 14.04.2022 ab 16:00 Uhr bis 18.04.2022',
-        'fr' : "<br>Pâques : Fermé le 14.04.2022 à partir de 16:00 jusqu'au 18.04.2022",
-        'it' : '<br>Pasqua: Chiuso 14.04.2022 dalle 16:00 al 18.04.2022',
-        'en-US' : '<br>Easter: Closed 14.04.2022 from 16:00 until 18.04.2022',
+        'de' : '<br>Auffahrt: Geschlossen 25.5.2022 ab 16 Uhr & 26.5.2022',
+        'fr' : "<br>Ascension : Fermé le 25.5.2022 à partir de 16h & le 26.5.2022",
+        'it' : '<br>Ascensione: Chiuso il 25.5.2022 dalle ore 16:00 e il 26.5.2022',
+        'en-US' : '<br>Ascension: Closed 25.5.2022 from 4pm & 26.5.2022',
     },
     1 : {
-        'de' : '<br>Ostern: 15.04.2022 & 18.04.2022 geschlossen',
-        'fr' : '<br>Pâques : 15.04.2022 & 18.04.2022 fermé',
-        'it' : '<br>Pasqua: 15.04.2022 & 18.04.2022 chiuso',
-        'en-US' : '<br>Easter: 15.04.2022 & 18.04.2022 closed',
+        'de' : '<br>Christi Himmelfahrt: 26.05.2022 geschlossen',
+        'fr' : '<br>Ascension : 26.05.2022 fermé',
+        'it' : '<br>Ascensione: 26.05.2022 chiuso',
+        'en-US' : '<br>Ascension: 26.05.2022 closed',
     }
 }
 
@@ -33,9 +33,9 @@ var textToDisplay = {
 //1. - ending date to stop hiding waiting times
 var hideWaitingTimes = {
     'ch' : [
-        ['2022-04-14 14:00:00', '2022-04-18 22:00:00']
+        ['2022-05-25 14:00:00', '2022-05-26 22:00:00']
     ],
     'eu' : [
-        ['2022-04-15 00:00:00', '2022-04-15 22:00:00'], ['2022-04-18 00:00:00', '2022-04-18 22:00:00']
+        ['2022-05-26 00:00:00', '2022-05-26 22:00:00']
     ]
 }
