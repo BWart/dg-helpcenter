a1e67c9afc994b4b75d18a10f72a5ae98fbe7b2d
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2022-05-31 18:12:31 +0200

    Pfingstmontag Holiday added. Adjusted account deletion infobox text.

--- assets/holidays.js
+++ assets/holidays.js
@@ -6,24 +6,24 @@
 //2. - ID of the text in textToDisplay 
 var showHolidays = {
     'ch' : [
-        ['2022-05-25 00:00:00', '2022-05-26 22:00:00', 0]
+        ['2022-06-03 00:00:00', '2022-06-06 22:00:00', 0]
     ],
     'eu' : [
-        ['2022-05-25 00:00:00', '2022-05-26 22:00:00', 1]
+        ['2022-06-03 00:00:00', '2022-06-06 22:00:00', 1]
     ]
 }
 var textToDisplay = {
     0 : {
-        'de' : '<br>Auffahrt: 25.05. 08:00 bis 16:00, 26.05. geschlossen',
-        'fr' : "<br>Ascension : 25.05. 08:00 à 16:00, 26.05. fermé",
-        'it' : '<br>Ascensione: 25.05. dalle 08:00 alle 16:00, 26.05. chiuso',
-        'en-US' : '<br>Ascension: 25.05. 08:00 to 16:00, 26.05. closed',
+        'de' : '<br>Pfingstmontag: 06.06. geschlossen',
+        'fr' : "<br>Lundi de Pentecôte : 06.06. fermé",
+        'it' : '<br>Lunedì di Pentecoste: 06.06. chiuso',
+        'en-US' : '<br>Whit Monday: 06.06. closed',
     },
     1 : {
-        'de' : '<br>Christi Himmelfahrt: 26.05.2022 geschlossen',
-        'fr' : '<br>Ascension : 26.05.2022 fermé',
-        'it' : '<br>Ascensione: 26.05.2022 chiuso',
-        'en-US' : '<br>Ascension: 26.05.2022 closed',
+        'de' : '<br>Pfingstmontag: 06.06. geschlossen',
+        'fr' : '<br>Lundi de Pentecôte : 06.06. fermé',
+        'it' : '<br>Lunedì di Pentecoste: 06.06. chiuso',
+        'en-US' : '<br>Whit Monday: 06.06. closed',
     }
 }
 
@@ -33,9 +33,9 @@ var textToDisplay = {
 //1. - ending date to stop hiding waiting times
 var hideWaitingTimes = {
     'ch' : [
-        ['2022-05-25 14:00:00', '2022-05-26 22:00:00']
+        ['2022-06-06 00:00:00', '2022-06-06 22:00:00']
     ],
     'eu' : [
-        ['2022-05-26 00:00:00', '2022-05-26 22:00:00']
+        ['2022-06-06 00:00:00', '2022-06-06 22:00:00']
     ]
 }
