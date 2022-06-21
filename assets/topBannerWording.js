ad997cc6b953745e4dddd56734a8dd1e633ab516
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2022-06-21 09:10:34 +0200

    added banner for technical issues in zendesk

--- assets/topBannerWording.js
+++ assets/topBannerWording.js
@@ -10,10 +10,10 @@ function fillTopBannerWording(){
       'en-US' : "Attention"
     },
     "outputTextBannerCH" : {
-      'de' : "Der Versand beträgt momentant 5-7 Tage.",
-      'fr' : "Les délais de livraison sont actuellement de 5 à 7 jours.",
-      'it' : "Il tempo di spedizione è attualmente di 5-7 giorni.",
-      'en-US' : "The shipping time is 5-7 days at the moment."
+      'de' : "Technische Störung: Kontaktanfragen über Chat, Telefon oder E-Mail funktionieren aktuell teilweise nicht.",
+      'fr' : "Dysfonctionnement technique : les demandes de contact par chat, téléphone ou e-mail ne fonctionnent actuellement que partiellement.",
+      'it' : "Malfunzionamento tecnico: le richieste di contatto via chat, telefono o e-mail non funzionano in alcuni casi.",
+      'en-US' : "Technical malfunction: Contact requests via chat, phone or e-mail are currently partially not working."
     },
     "outputHeadingBannerDE" : {
       'de' : "Achtung",
@@ -22,10 +22,10 @@ function fillTopBannerWording(){
       'en-US' : "Attention"
     },
     "outputTextBannerDE" : {
-      'de' : "Der Versand beträgt momentant 25-27 Tage.",
-      'fr' : "Les délais de livraison sont actuellement de 25 à 27 jours.",
-      'it' : "Il tempo di spedizione è attualmente di 25-27 giorni.",
-      'en-US' : "The shipping time is 25-27 days at the moment."
+      'de' : "Technische Störung: Kontaktanfragen über Chat, Telefon oder E-Mail funktionieren aktuell teilweise nicht.",
+      'fr' : "Dysfonctionnement technique : les demandes de contact par chat, téléphone ou e-mail ne fonctionnent actuellement que partiellement.",
+      'it' : "Malfunzionamento tecnico: le richieste di contatto via chat, telefono o e-mail non funzionano in alcuni casi.",
+      'en-US' : "Technical malfunction: Contact requests via chat, phone or e-mail are currently partially not working."
     },
     "outputHeadingBannerAT" : {
       'de' : "Achtung",
@@ -34,10 +34,10 @@ function fillTopBannerWording(){
       'en-US' : "Attention"
     },
     "outputTextBannerAT" : {
-      'de' : "Der Versand beträgt momentant 35-37 Tage.",
-      'fr' : "Les délais de livraison sont actuellement de 35 à 37 jours.",
-      'it' : "Il tempo di spedizione è attualmente di 35-37 giorni.",
-      'en-US' : "The shipping time is 35-37 days at the moment."
+      'de' : "Technische Störung: Kontaktanfragen über Chat, Telefon oder E-Mail funktionieren aktuell teilweise nicht.",
+      'fr' : "Dysfonctionnement technique : les demandes de contact par chat, téléphone ou e-mail ne fonctionnent actuellement que partiellement.",
+      'it' : "Malfunzionamento tecnico: le richieste di contatto via chat, telefono o e-mail non funzionano in alcuni casi.",
+      'en-US' : "Technical malfunction: Contact requests via chat, phone or e-mail are currently partially not working."
     },
     
     }
