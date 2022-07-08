67ec096c081683966c9c7a620319bce046b0f9cd
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2022-07-08 19:01:28 +0200

    info for longer wait time and phone deactivated for PE

--- assets/holidaysAndSwitches.js
+++ assets/holidaysAndSwitches.js
@@ -19,6 +19,24 @@ var showHolidays = {
     ],
     'eu' : [
         ['2022-06-15 00:00:00', '2022-06-16 22:00:00', 1]
+    ],
+    'chpe' : [
+        ['2022-07-08 17:00:00', '2022-07-11 06:00:00', 2],
+        ['2022-07-11 17:00:00', '2022-07-12 06:00:00', 2],
+        ['2022-07-12 17:00:00', '2022-07-13 06:00:00', 2],
+        ['2022-07-13 17:00:00', '2022-07-14 06:00:00', 2],
+        ['2022-07-14 17:00:00', '2022-07-15 06:00:00', 2],
+        ['2022-07-15 17:00:00', '2022-07-18 06:00:00', 2],
+        ['2022-07-18 17:00:00', '2022-07-19 06:00:00', 2],
+        ['2022-07-19 17:00:00', '2022-07-20 06:00:00', 2],
+        ['2022-07-20 17:00:00', '2022-07-21 06:00:00', 2],
+        ['2022-07-21 17:00:00', '2022-07-22 06:00:00', 2],
+        ['2022-07-22 17:00:00', '2022-07-25 06:00:00', 2],
+        ['2022-07-25 17:00:00', '2022-07-26 06:00:00', 2],
+        ['2022-07-26 17:00:00', '2022-07-27 06:00:00', 2],
+        ['2022-07-27 17:00:00', '2022-07-28 06:00:00', 2],
+        ['2022-07-28 17:00:00', '2022-07-29 06:00:00', 2],
+        ['2022-07-29 17:00:00', '2022-08-01 06:00:00', 2]
     ]
 }
 //THE TEXT TO BE DISPLAYED ACCORDING TO THE ID SET ABOVE
@@ -35,6 +53,12 @@ var textToDisplay = {
         'fr' : '<br>Fête-Dieu : 16.06. fermé',
         'it' : '<br>Corpus Christi: 16.06. chiuso',
         'en-US' : '<br>Corpus Christi: 16.06. closed',
+    },
+    2 : {
+        'de' : '<br><font color="red">Hinweis:</font> Eine direkte Produktberatung bieten wir nur während den Öffnungszeiten an. Ausserhalb können Sie sich an unsere Community wenden.',
+        'fr' : '<br><font color="red">Remarque :</font> Nous ne proposons un conseil direct sur les produits que pendant les heures d\'ouverture. En dehors de ces heures, vous pouvez vous adresser à notre communauté.',
+        'it' : '<br><font color="red">Nota bene:</font> Offriamo consulenza diretta sui prodotti solo durante gli orari di apertura. Al di fuori degli orari di apertura, potete contattare la nostra comunità.',
+        'en-US' : '<br><font color="red">Note:</font> We only offer direct product advice during opening hours. Outside of these hours, you can contact our community.'
     }
 }
 
