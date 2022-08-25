7fa4cb0c6ccfbf4b0ff07caa4722dc742e237daf
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2022-08-25 17:53:01 +0200

    banner warning connect

--- assets/topBannerWording.js
+++ assets/topBannerWording.js
@@ -4,16 +4,16 @@ function fillTopBannerWording(){
 
     //Banner
     "outputHeadingBannerCH" : {
-      'de' : "Achtung",
-      'fr' : "Attention",
-      'it' : "Attenzione",
-      'en-US' : "Attention"
+      'de' : "Technische Störung",
+      'fr' : "Panne technique",
+      'it' : "Guasto tecnico",
+      'en-US' : "Technical malfunction"
     },
     "outputTextBannerCH" : {
-      'de' : "Technische Störung: Kontaktanfragen über Chat, Telefon oder E-Mail funktionieren aktuell teilweise nicht.",
-      'fr' : "Dysfonctionnement technique : les demandes de contact par chat, téléphone ou e-mail ne fonctionnent actuellement que partiellement.",
-      'it' : "Malfunzionamento tecnico: le richieste di contatto via chat, telefono o e-mail non funzionano in alcuni casi.",
-      'en-US' : "Technical malfunction: Contact requests via chat, phone or e-mail are currently partially not working."
+      'de' : "Unsere Telefonleitung ist aufgrund einer technischen Störung zurzeit nicht verfügbar",
+      'fr' : "Notre ligne téléphonique est actuellement indisponible en raison d'un problème technique.",
+      'it' : "La nostra linea telefonica non è attualmente disponibile a causa di un guasto tecnico.",
+      'en-US' : "Our telephone line is currently unavailable due to a technical malfunction"
     },
     "outputHeadingBannerDE" : {
       'de' : "Achtung",
