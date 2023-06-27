dd965be5a256ac8c5bbd0686a40c603b12cc6886
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2023-06-27 11:06:14 +0200

    callback reactivated

--- assets/callbackFunctions.js
+++ assets/callbackFunctions.js
@@ -1,6 +1,5 @@
 function callbackCheck(){
-  console.log("ENTER CALLBACKCHECK")
-    return false;
+    //return false;
     //CallbackCheck Private CH
     if (reasonsForCallback.indexOf(requestReasonTag) >= 0 && localStorage.getItem("callbackGroup5") == "true" && customerType == "private-customer" && (portal == 'helpcenter.digitec.ch' || portal == 'helpcenter.galaxus.ch')){
       //console.log("callbackcheck private true")
