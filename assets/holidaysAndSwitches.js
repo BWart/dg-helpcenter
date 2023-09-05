32295bd758c0a0bf66c44086063e562de7508076
Milovan Djokic <milovan.djokic@digitecgalaxus.ch>
2023-09-05 11:27:40 +0200

    Beschriebung ergänzt, URL gelöscht

--- assets/holidaysAndSwitches.js
+++ assets/holidaysAndSwitches.js
@@ -494,6 +494,7 @@ var hideWaitingTimes = {
 }
 
 //Hiding waiting times in case of stuck calls/chats
+//format aus waiting nehmen https://py-waittimes-web.prod.pub.devinite.com/waitTimes.js
 let waitTimesToHideCall = []
 let waitTimesToHideChat = []
 
