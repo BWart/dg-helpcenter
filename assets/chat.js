d400c0474558f0150a3da2a656b4db02253a2bcc
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2023-09-29 11:14:51 +0200

    more testing

--- assets/chat.js
+++ assets/chat.js
@@ -58,15 +58,16 @@ function openChat(){
     
     zE("messenger", "show")
 
+    console.log(requestReasonTag)
+    zE("messenger:set", "conversationTags", [requestReasonTag])
+
     zE("messenger:set", "conversationFields", [
-        {id: "360007585419", value: 123456},
+        {id: "360007585419", value: "95774269"},
         //{id: "360021132800", value: "webform_case_connect_b2b"}
-        {id: "360021132820", value: 763066030},
+        {id: "360021132820", value: "0763066030"},
         //{id: "123", value: "adfasdfasdf"}
     ])
 
-    console.log(requestReasonTag)
-    zE("messenger:set", "conversationTags", [requestReasonTag])
     zE("messenger", "open")
 }
 
