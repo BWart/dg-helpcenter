62fd46cf514be507d011b34fa64282283fbc7b39
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2023-11-21 20:02:33 +0100

    opening times text for EU for Chat

--- assets/dynamicWording.js
+++ assets/dynamicWording.js
@@ -240,20 +240,20 @@ function filldynamicWording(extraPrivateNumber, extraPrivateNumberText){
                 shopLink = "https://www.galaxus.fr/"
 
                 //Öffnungszeiten
-                tagePrivatkundeDE = "";
-                tagePrivatkundeFR = "";
-                tagePrivatkundeIT = "";
-                tagePrivatkundeEN = "";
-                tagePrivatkundeNL = "";
-                tagePrivatkunde2DE = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2FR = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2IT = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2EN = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2NL = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                uhrzeitPrivatkunde = "";
-                uhrzeitPrivatkunde2 = "";
-                uhrzeitPrivatkundePe = "";
-                uhrzeitPrivatkunde2Pe = "";
+                tagePrivatkundeDE = "Mo - Fr ";
+                tagePrivatkundeFR = "Lu - Ve ";
+                tagePrivatkundeIT = "Lun - Ven ";
+                tagePrivatkundeEN = "Mo - Fr ";
+                tagePrivatkundeNL = "Ma - Vr ";
+                tagePrivatkunde2DE = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2FR = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2IT = " | Sat ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2EN = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2NL = " | Za ";  //WICHTIG: braucht leading " | " wenn befüllt
+                uhrzeitPrivatkunde = "08:00 - 19:00";
+                uhrzeitPrivatkunde2 = "09:00 - 17:00";
+                uhrzeitPrivatkundePe = "08:00 - 19:00";
+                uhrzeitPrivatkunde2Pe = "09:00 - 17:00";
                 // TODO: LINKS AUF KORREKTHEIT PRÜFEN
                 privacynoticeDE = '<a style="color:white;font-size:13px;" href="https://www.galaxus.fr/wiki/6182" target="blank">Datenschutzerklärung</a>';
                 privacynoticeFR = '<a style="color:white;font-size:13px;" href="https://www.galaxus.fr/wiki/6182" target="blank">Déclaration de protection des données</a>';
@@ -284,20 +284,20 @@ function filldynamicWording(extraPrivateNumber, extraPrivateNumberText){
                 shopLink = "https://www.galaxus.it/"
 
                 //Öffnungszeiten
-                tagePrivatkundeDE = "";
-                tagePrivatkundeFR = "";
-                tagePrivatkundeIT = "";
-                tagePrivatkundeEN = "";
-                tagePrivatkundeNL = "";
-                tagePrivatkunde2DE = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2FR = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2IT = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2EN = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2NL = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                uhrzeitPrivatkunde = "";
-                uhrzeitPrivatkunde2 = "";
-                uhrzeitPrivatkundePe = "";
-                uhrzeitPrivatkunde2Pe = "";
+                tagePrivatkundeDE = "Mo - Fr ";
+                tagePrivatkundeFR = "Lu - Ve ";
+                tagePrivatkundeIT = "Lun - Ven ";
+                tagePrivatkundeEN = "Mo - Fr ";
+                tagePrivatkundeNL = "Ma - Vr ";
+                tagePrivatkunde2DE = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2FR = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2IT = " | Sat ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2EN = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2NL = " | Za ";  //WICHTIG: braucht leading " | " wenn befüllt
+                uhrzeitPrivatkunde = "08:00 - 19:00";
+                uhrzeitPrivatkunde2 = "09:00 - 17:00";
+                uhrzeitPrivatkundePe = "08:00 - 19:00";
+                uhrzeitPrivatkunde2Pe = "09:00 - 17:00";
                 // TODO: LINKS AUF KORREKTHEIT PRÜFEN
                 privacynoticeDE = '<a style="color:white;font-size:13px;" href="https://www.galaxus.it/wiki/6182" target="blank">Datenschutzerklärung</a>';
                 privacynoticeFR = '<a style="color:white;font-size:13px;" href="https://www.galaxus.it/wiki/6182" target="blank">Déclaration de protection des données</a>';
@@ -328,20 +328,20 @@ function filldynamicWording(extraPrivateNumber, extraPrivateNumberText){
                 shopLink = "https://www.galaxus.be/"
 
                 //Öffnungszeiten
-                tagePrivatkundeDE = "";
-                tagePrivatkundeFR = "";
-                tagePrivatkundeIT = "";
-                tagePrivatkundeEN = "";
-                tagePrivatkundeNL = "";
-                tagePrivatkunde2DE = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2FR = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2IT = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2EN = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2NL = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                uhrzeitPrivatkunde = "";
-                uhrzeitPrivatkunde2 = "";
-                uhrzeitPrivatkundePe = "";
-                uhrzeitPrivatkunde2Pe = "";
+                tagePrivatkundeDE = "Mo - Fr ";
+                tagePrivatkundeFR = "Lu - Ve ";
+                tagePrivatkundeIT = "Lun - Ven ";
+                tagePrivatkundeEN = "Mo - Fr ";
+                tagePrivatkundeNL = "Ma - Vr ";
+                tagePrivatkunde2DE = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2FR = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2IT = " | Sat ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2EN = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2NL = " | Za ";  //WICHTIG: braucht leading " | " wenn befüllt
+                uhrzeitPrivatkunde = "08:00 - 19:00";
+                uhrzeitPrivatkunde2 = "09:00 - 17:00";
+                uhrzeitPrivatkundePe = "08:00 - 19:00";
+                uhrzeitPrivatkunde2Pe = "09:00 - 17:00";
                 // TODO: LINKS AUF KORREKTHEIT PRÜFEN
                 privacynoticeDE = '<a style="color:white;font-size:13px;" href="https://www.galaxus.it/wiki/6182" target="blank">Datenschutzerklärung</a>';
                 privacynoticeFR = '<a style="color:white;font-size:13px;" href="https://www.galaxus.it/wiki/6182" target="blank">Déclaration de protection des données</a>';
@@ -372,20 +372,20 @@ function filldynamicWording(extraPrivateNumber, extraPrivateNumberText){
                 shopLink = "https://www.galaxus.nl/"
 
                 //Öffnungszeiten
-                tagePrivatkundeDE = "";
-                tagePrivatkundeFR = "";
-                tagePrivatkundeIT = "";
-                tagePrivatkundeEN = "";
-                tagePrivatkundeNL = "";
-                tagePrivatkunde2DE = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2FR = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2IT = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2EN = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                tagePrivatkunde2NL = "";  //WICHTIG: braucht leading " |  " wenn befüllt
-                uhrzeitPrivatkunde = "";
-                uhrzeitPrivatkunde2 = "";
-                uhrzeitPrivatkundePe = "";
-                uhrzeitPrivatkunde2Pe = "";
+                tagePrivatkundeDE = "Mo - Fr ";
+                tagePrivatkundeFR = "Lu - Ve ";
+                tagePrivatkundeIT = "Lun - Ven ";
+                tagePrivatkundeEN = "Mo - Fr ";
+                tagePrivatkundeNL = "Ma - Vr ";
+                tagePrivatkunde2DE = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2FR = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2IT = " | Sat ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2EN = " | Sa ";  //WICHTIG: braucht leading " | " wenn befüllt
+                tagePrivatkunde2NL = " | Za ";  //WICHTIG: braucht leading " | " wenn befüllt
+                uhrzeitPrivatkunde = "08:00 - 19:00";
+                uhrzeitPrivatkunde2 = "09:00 - 17:00";
+                uhrzeitPrivatkundePe = "08:00 - 19:00";
+                uhrzeitPrivatkunde2Pe = "09:00 - 17:00";
                 // TODO: LINKS AUF KORREKTHEIT PRÜFEN
                 privacynoticeDE = '<a style="color:white;font-size:13px;" href="https://www.galaxus.it/wiki/6182" target="blank">Datenschutzerklärung</a>';
                 privacynoticeFR = '<a style="color:white;font-size:13px;" href="https://www.galaxus.it/wiki/6182" target="blank">Déclaration de protection des données</a>';
