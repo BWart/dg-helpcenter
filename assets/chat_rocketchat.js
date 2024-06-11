efd4f35af3fe8de725dcbf8f54ac26a6b1c32f63
Bastian Wartmann <bastian.wartmann@digitecgalaxus.ch>
2024-06-11 17:42:52 +0200

    Widget functional with custom field

--- assets/chat_rocketchat.js
+++ assets/chat_rocketchat.js
@@ -1,4 +1,4 @@
-(function(w, d, s, u) {
+    (function(w, d, s, u) {
 		w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
 		var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
 		j.async = true; j.src = 'https://digitecgalaxus.rocket.chat/livechat/rocketchat-livechat.min.js?_=201903270000';
@@ -6,14 +6,8 @@
 	})(window, document, 'script', 'https://digitecgalaxus.rocket.chat/livechat');
 
     RocketChat(function() {
-        this.setGuestEmail('sample@rocket.chat')
-        this.setDepartment('Chat EU DE');
-        this.setCustomField('ordernumber', '12345678')
+        this.setGuestEmail('sample@rocket.chat');
+        this.setDepartment('Chat EU Multilingual');
         this.maximizeWidget();
-    });
-
-    RocketChat(function(){
-        this.onChatStarted(function(){
-            this.setCustomField('ordernumber', '12345678')
-        });
+        this.setCustomField('ordernumber', '999');
     });
